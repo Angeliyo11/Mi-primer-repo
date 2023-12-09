e496d5c89d7616d183645a2204d68454dc89b56b
angeliyo <angelonce60@gmail.com>
2023-12-09 11:24:51 -0500

    Archivo modificado de mi juego

--- Archivo configurado de mi primer progreso.py
+++ Archivo configurado de mi primer progreso.py
@@ -1,10 +1,35 @@
 print("HOLA BIENVENIDOS A MI PRIMER JUEGO")
 print("Creado por 'zlAngel'")
 print(" ")
-print("¡Espero que te diviertas!")
+print("Espero que te diviertas!")
 print(" ")
 print("RECUERDA RESPETAR TU TURNO")
 print(" ")
-while True:
-    a= int(input("Jugador 1: 1=Piedra, 2=Papel, 3=Tijera"))
-    b= int(input("Jugador 2: 1=Piedra, 2=Papel, 3=Tijera"))
+
+import random
+
+choices = ["piedra", "papel", "tijera"]
+attempts = 5
+
+for _ in range(attempts):
+    user_choice = input("Elige piedra, papel o tijera: ").lower()
+    computer_choice = random.choice(choices)
+    
+    if user_choice not in choices:
+        print("Eliga una opción valida ._.")
+        continue
+    
+    print(f"Usted eligió: {user_choice}")
+    print(f"La CPU eligió: {computer_choice}")
+    
+    if user_choice == computer_choice:
+        print("Oh no esto es un empata O.o")
+    elif (user_choice == "piedra" and computer_choice == "tijera") or (user_choice == "papel" and computer_choice == "piedra") or (user_choice == "tijera" and computer_choice == "papel"):
+        print("YEAH Ganaste que crack :3")
+    else:
+        print("OHHH Te gano la  :(")
+
+print("End Game")
+
+
+
